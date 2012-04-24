5e52aa88642e30d7b2420bd683484f2388bc9d6c
Nick Stenning <nick@whiteink.com>
2012-04-24 20:48:20 +0100

    Bump version -> 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'honcho',
-    version = '0.0.2',
+    version = '0.0.3',
     packages = find_packages(),
 
     install_requires = [],
