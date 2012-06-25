cd4fadd1fe5bb5a4a03294f2d82b8253493abd7d
Nick Stenning <nick@whiteink.com>
2012-06-25 16:05:30 +0100

    Add support for -p,--port and -c,--concurrency à la Foreman

--- honcho/command.py
+++ honcho/command.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import argparse
+from collections import defaultdict
 import logging
 import os
 import re
@@ -54,6 +55,15 @@ def read_env(args):
 
             os.environ[key] = val
 
+def parse_concurrency(desc):
+    result = defaultdict(lambda: 1)
+    if desc is None:
+        return result
+    for item in desc.split(','):
+        key, concurrency = item.split('=', 1)
+        result[key] = int(concurrency)
+    return result
+
 def check(args):
     procfile = make_procfile(args.procfile)
 
@@ -76,8 +86,16 @@ def start(args):
     if not procfile:
         sys.exit(1)
 
+    port = args.port
+    concurrency = parse_concurrency(args.concurrency)
+
     for name, cmd in procfile.commands.iteritems():
-        process_manager.add_process(name, cmd)
+        for i in xrange(1, concurrency[name] + 1):
+            n = '{name}.{i}'.format(**vars())
+            os.environ['PORT'] = str(port)
+            process_manager.add_process(n, cmd)
+            port += 1
+        port += 1000
 
     process_manager.loop()
 
@@ -88,6 +106,8 @@ def main():
 parser = argparse.ArgumentParser(description='Manage Procfile-based applications')
 parser.add_argument('-f', '--procfile', help='Default: Procfile', default='Procfile')
 parser.add_argument('-d', '--app-root', help='Default: Procfile directory')
+parser.add_argument('-p', '--port', help='Default: 5000', type=int, default=5000)
+parser.add_argument('-c', '--concurrency', help='The number of each process type to run. The value passed in should be in the format "process=num,process=num"')
 
 subparsers = parser.add_subparsers(help='Commands')
 parser_check = subparsers.add_parser('check', help="Validate your application's Procfile")
