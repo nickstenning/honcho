e3d0c5ad7c6b3df1e9c09c6bd6986728dbeb330e
Nick Stenning <nick@whiteink.com>
2012-09-16 20:53:59 +0200

    Python 2.6 compatibility for tests

--- test/helpers.py
+++ test/helpers.py
@@ -1,12 +1,36 @@
 import os
-import subprocess
+import re
+from subprocess import Popen, PIPE, CalledProcessError
 from nose.tools import *
 from mock import *
 
 FIXTURE_ROOT = os.path.join(os.path.dirname(__file__), 'fixtures')
 
+
+if not 'assert_regexp_matches' in globals():
+
+    def assert_regexp_matches(text, expected_regexp, msg=None):
+        """Fail the test unless the text matches the regular expression."""
+        if isinstance(expected_regexp, basestring):
+            expected_regexp = re.compile(expected_regexp)
+        if not expected_regexp.search(text):
+            msg = msg or "Regexp didn't match"
+            msg = '%s: %r not found in %r' % (msg, expected_regexp.pattern, text)
+            raise AssertionError(msg)
+
 def get_honcho_output(args):
     os.chdir(FIXTURE_ROOT)
     cmd = ['honcho']
     cmd.extend(args)
-    return subprocess.check_output(cmd)
+
+    # The below is mostly copy-pasted from subprocess.py's check_output (to
+    # support python 2.6)
+
+    process = Popen(cmd, stdout=PIPE)
+    output, _ = process.communicate()
+    retcode = process.poll()
+
+    if retcode:
+        raise CalledProcessError(retcode, cmd, output=output)
+
+    return output
