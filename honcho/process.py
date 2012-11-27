dbeb84abc81841021269ab023d6de1265f13e131
Mike Axiak <maxiak@hubspot.com>
2012-11-27 01:09:25 -0500

    Don't prematurely exit the parent process before the buffered stdout is finished.

--- honcho/process.py
+++ honcho/process.py
@@ -139,7 +139,6 @@ class ProcessManager(object):
             return False
 
         self._terminating = True
-
         print("sending SIGTERM to all processes", file=self.system_printer)
         for proc in self.processes:
             if proc.poll() is None:
@@ -155,7 +154,6 @@ class ProcessManager(object):
 
         signal.signal(signal.SIGALRM, kill)
         signal.alarm(5)
-        self._exit_with_code()
 
     def _exit_with_code(self):
         if len(self.processes) == 1 and self.processes[0].returncode is not None:
