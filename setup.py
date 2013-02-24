c15d28788a1404d624704e3c275230fc73ac6f0a
Nick Stenning <nick@whiteink.com>
2013-02-24 20:02:02 +0000

    Replace README.md with README.rst

--- setup.py
+++ setup.py
@@ -9,6 +9,12 @@ if sys.version_info[:2] < (2, 7):
     requirements.append('argparse')
     requirements.append('ordereddict')
 
+HERE = os.path.dirname(__file__)
+try:
+    long_description = open(os.path.join(HERE, 'README.rst')).read()
+except:
+    long_description = None
+
 setup(
     name='honcho',
     version=__version__,
@@ -20,6 +26,7 @@ setup(
     author_email='nick@whiteink.com',
     url='https://github.com/nickstenning/honcho',
     description='Honcho: a python clone of Foreman. For managing Procfile-based applications.',
+    long_description=long_description,
     license='MIT',
     keywords='sysadmin process procfile',
 
