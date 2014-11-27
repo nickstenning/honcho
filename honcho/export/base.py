66d9503223351e496817d69b75bc18a9651fe010
Marc Abramowitz <marc@marc-abramowitz.com>
2014-11-27 12:12:31 -0800

    Export: Append \n to EOF is not already present

--- honcho/export/base.py
+++ honcho/export/base.py
@@ -57,6 +57,9 @@ class BaseExport(object):
     def _write(self, filename, content):
         path = os.path.join(self.options.location, filename)
 
+        if not content.endswith('\n'):
+            content += '\n'
+
         try:
             open(path, 'w').write(content)
         except IOError:
