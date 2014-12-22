25a1f86c265a4c77c62ea54b2d41285976619619
Nick Stenning <nick@whiteink.com>
2014-12-22 12:37:12 +0000

    Remove "test_awkward" integration test
    The functionality this tests is already amply covered by unit tests of
    Process and Printer, and the integration test is much more fragile.

--- honcho/test/fixtures/awkward.py
+++
@@ -1,7 +0,0 @@
-from __future__ import print_function
-import sys
-
-if __name__ == '__main__':
-    for i in range(10):
-        print("Hello with no line break", end='')
-        sys.stdout.flush()
