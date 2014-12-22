4ea136e597895bedb6ef0a1daad740c4825381b1
Nick Stenning <nick@whiteink.com>
2014-12-22 13:09:13 +0000

    Add a complete TestEnv context manager to helpers
    This can be used to set up a directory in which to test an instance of
    honcho, without relying on the contents of the fixtures directory, which
    is getting unwieldy.

--- honcho/test/helpers.py
+++ honcho/test/helpers.py
@@ -1,5 +1,7 @@
 import os
 import re
+import shutil
+import tempfile
 import unittest
 from honcho import environ
 from subprocess import Popen, PIPE
@@ -54,6 +56,57 @@ class TestCase(unittest.TestCase):
                 standardMsg = '%s unexpectedly found in %s' % (repr(member),  # noqa
                                                                repr(container))
                 self.fail(self._formatMessage(msg, standardMsg))
+
+
+class TestEnv(object):
+    def __init__(self, files=None):
+        self.root = None
+        self._files = files
+
+    def cleanup(self):
+        if self.root is not None:
+            shutil.rmtree(self.root)
+            self.root = None
+
+    def setup(self):
+        self.cleanup()
+
+        self.root = tempfile.mkdtemp()
+        if self._files is not None:
+            for n, contents in self._files.items():
+                with open(os.path.join(self.root, n), 'w') as fp:
+                    fp.write(contents)
+
+    def path(self, *args):
+        return os.path.join(self.root, *args)
+
+    def run_honcho(self, args):
+        cwd = os.getcwd()
+        os.chdir(self.root)
+
+        cmd = ['honcho']
+        cmd.extend(args)
+
+        # The below is mostly copy-pasted from subprocess.py's check_output (to
+        # support python 2.6)
+
+        process = Popen(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+        output, error = process.communicate()
+        retcode = process.returncode
+
+        os.chdir(cwd)
+
+        return retcode, output, error
+
+    def __enter__(self):
+        self.setup()
+        return self
+
+    def __exit__(self, *args):
+        self.cleanup()
+
+    def __del__(self):
+        self.cleanup()
 
 
 def get_honcho_output(args):
