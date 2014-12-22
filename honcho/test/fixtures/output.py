66dd59bc1db19984f80266c76ab492b617b53b84
Nick Stenning <nick@whiteink.com>
2014-12-22 21:31:40 +0000

    Remove fixtures directory
    None of these are used any more.

--- honcho/test/fixtures/output.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-
-if __name__ == '__main__':
-    sys.stdout.write('some normal output\n')
-    sys.stdout.flush()
-    sys.stderr.write('and then write to stderr\n')
-    sys.stderr.flush()
