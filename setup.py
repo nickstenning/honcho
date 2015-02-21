4db284d176bef7eae2bc1238edb2722f0e768d63
Nick Stenning <nick@whiteink.com>
2015-02-21 20:25:43 +0000

    Update trove classifiers

--- setup.py
+++ setup.py
@@ -25,9 +25,21 @@ setup(
     license='MIT',
     keywords='sysadmin process procfile',
     classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
     install_requires=[],
     extras_require={
