0884c616566231bfc08975cae749142722873352
requires.io <support@requires.io>
2015-02-24 12:49:10 +0000

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     extras_require={
         ':python_version=="2.6"': ['argparse', 'ordereddict'],
         ':sys_platform=="win32"': ['colorama'],
-        'export:python_version in "3.0,3.1,3.2"': ['jinja2>=2.6,<2.7'],
+        'export:python_version in "3.0,3.1,3.2"': ['jinja2>=2.7,<2.8'],
         'export:python_version not in "3.0,3.1,3.2"': ['jinja2>=2.7,<2.8'],
     },
     entry_points={
