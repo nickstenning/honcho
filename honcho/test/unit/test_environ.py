nickstenning/honcho

commit 08063ea0dfead6850c243f5a2b5d917790252d6a

Allow more types of characters in .env values

honcho/environ.py
@@ -78,13 +78,15 @@ def parse(content):
     values = {}
     for line in content.splitlines():
         lexer = shlex.shlex(line, posix=True)
-        lexer.wordchars += '/.+-():'
         tokens = list(lexer)
 
         # parses the assignment statement
-        if len(tokens) != 3:
+        if len(tokens) < 3:
             continue
-        name, op, value = tokens
+
+        name, op = tokens[:2]
+        value = ''.join(tokens[2:])
+
         if op != '=':
             continue
         if not re.match(r'[A-Za-z_][A-Za-z_0-9]*', name):

honcho/test/unit/test_environ.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 import textwrap
 from ..helpers import TestCase
 
@@ -66,6 +68,41 @@ ENVFILE_FIXTURES = [
         """,
         {'MYVAR': '"escaped"'}
     ],
+    [
+        # At-sign in value
+        r"""
+        MYVAR=user@domain.com
+        """,
+        {'MYVAR': 'user@domain.com'}
+    ],
+    [
+        # Much punctuation in value
+        r"""
+        MYVAR=~pun|u@|0n$=
+        """,
+        {'MYVAR': '~pun|u@|0n$='}
+    ],
+    [
+        # Unicode values
+        r"""
+        MYVAR=⋃ñᴉ—☪ó∂ǝ
+        """,
+        {'MYVAR': '⋃ñᴉ—☪ó∂ǝ'}
+    ],
+    [
+        # Unicode keys
+        r"""
+        ṀẎṾẠṚ=value
+        """,
+        {}
+    ],
+    [
+        # Quoted space in value
+        r"""
+        MYVAR='sp ace'
+        """,
+        {'MYVAR': 'sp ace'}
+    ]
 ]
 
 PROCFILE_FIXTURES = [

commit 920b0b012007d2f6c17fe91b52cc9244478f48b7

Add support for multiline strings in .env
We previously supported this but regressed support in
08063ea.

Fixes #128.
Closes #129.

honcho/environ.py
@@ -91,6 +91,8 @@ def parse(content):
             continue
         if not re.match(r'[A-Za-z_][A-Za-z_0-9]*', name):
             continue
+
+        value = value.decode('string_escape')
         values[name] = value
 
     return values

honcho/test/unit/test_environ.py
@@ -102,7 +102,20 @@ ENVFILE_FIXTURES = [
         MYVAR='sp ace'
         """,
         {'MYVAR': 'sp ace'}
-    ]
+    ],
+    [
+        # Escaped characters in value
+        r"""
+        TABS='foo\tbar'
+        NEWLINES='foo\nbar'
+        VTAB='foo\vbar'
+        DOLLAR='foo\$bar'
+        """,
+        {'TABS': 'foo\tbar',
+         'NEWLINES': 'foo\nbar',
+         'VTAB': 'foo\vbar',
+         'DOLLAR': 'foo\\$bar'}
+    ],
 ]
 
 PROCFILE_FIXTURES = [