6928ac16ca5a5c9fb975e3eda3351f28941c8576
Michal Migurski <mike@teczno.com>
2015-06-18 22:58:55 -0700

    Added failing tests demonstrating incorrect upstart output

--- honcho/test/integration/test_export.py
+++ honcho/test/integration/test_export.py
@@ -1,4 +1,4 @@
-import os
+import os, re
 
 from ..helpers import TestCase
 from ..helpers import TestEnv
@@ -25,10 +25,17 @@ class TestExport(TestCase):
 
     def test_export_upstart(self):
         with TestEnv(files) as env:
+            env_path = os.path.join(env.root, '.env')
+
+            with open(env_path, 'w') as file:
+                # Add four environment variables to configuration.
+                file.write('\n'.join(('A=a', 'B="b"', 'C=c c', 'D="d d"')))
+
             ret, out, err = env.run_honcho([
                 'export',
                 'upstart',
                 env.path('elephant'),
+                '-e', env_path,
                 '-a', 'trunk',
             ])
 
@@ -38,3 +45,11 @@ class TestExport(TestCase):
                              'trunk-web-1.conf'):
                 expected = env.path('elephant', filename)
                 self.assertTrue(os.path.exists(expected))
+            
+            with open(env.path('elephant', 'trunk-web-1.conf')) as file:
+                content = file.read()
+                
+                self.assertTrue(re.search(r'\bA=a\b', content), 'A should be "a"')
+                self.assertTrue(re.search(r'\bB=b\b', content), 'B should be "b"')
+                self.assertFalse(re.search(r'\bC=cc\b', content), 'C should be correctly-quoted "c c"')
+                self.assertFalse(re.search(r"'cd [^']+\bexport D='d d';[^']+'", content), 'D should be correctly-quoted "d d"')
