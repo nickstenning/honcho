d0b551ff221c4e4f5c772277e5e005d8b3ff0125
Nick Stenning <nick@whiteink.com>
2016-04-10 13:09:13 +0200

    Fix quoting of special env var values in Upstart configs (#171)
    As reported by #151, the quoting of environment variable values in
    Upstart configs is currently broken in the general case.
    
    Correctly quoting variable values in the current context (i.e. already
    inside single quotes) is possible, but extremely messy, and results in
    hugely nested quotes when those values contain single quotes.
    
    Instead, move the definition of the environment variables into the body
    of the Upstart config using the `env` directive, and add the `-m` option
    to `su(1)` so that environment variables are passed through to the shell
    process.
    
    Closes #151.
    Closes #154.

--- tests/integration/test_export.py
+++ tests/integration/test_export.py
@@ -37,3 +37,37 @@ def test_export_upstart(testenv):
                      'trunk-web-1.conf'):
         expected = testenv.path('elephant', filename)
         assert os.path.exists(expected)
+
+
+@pytest.mark.parametrize('testenv', [{
+    'Procfile': "web: python web.py",
+    '.env': """
+NORMAL=ok
+SQ_SPACES='sqspace sqspace'
+DQ_SPACES="dqspace dqspace"
+SQ="it's got single quotes"
+DQ='it has "double" quotes'
+EXCL='an exclamation mark!'
+SQ_DOLLAR='costs $UNINTERPOLATED amount'
+DQ_DOLLAR="costs $UNINTERPOLATED amount"
+"""
+}], indirect=True)
+def test_export_upstart_environment(testenv):
+    ret, out, err = testenv.run_honcho([
+        'export',
+        'upstart',
+        testenv.path('test'),
+        '-a', 'envvars',
+    ])
+
+    assert ret == 0
+
+    lines = open(testenv.path('test', 'envvars-web-1.conf')).readlines()
+    assert 'env NORMAL=ok\n' in lines
+    assert "env SQ_SPACES='sqspace sqspace'\n" in lines
+    assert "env DQ_SPACES='dqspace dqspace'\n" in lines
+    assert "env SQ='it'\"'\"'s got single quotes'\n" in lines
+    assert "env DQ='it has \"double\" quotes'\n" in lines
+    assert "env EXCL='an exclamation mark!'\n" in lines
+    assert "env SQ_DOLLAR='costs $UNINTERPOLATED amount'\n" in lines
+    assert "env DQ_DOLLAR='costs $UNINTERPOLATED amount'\n" in lines
