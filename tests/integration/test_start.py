bf64b59acba360912be18dccd7f33205ed6d33a6
Marc Krull <marc.krull@cloudlock.com>
2016-04-13 02:15:01 -0400

    Adds two integration tests to prove order-dependent behavior of the procfile (-f) argument relative to a command (I've used start, but the behavior looks to be consistent across all commands)

--- tests/integration/test_start.py
+++ tests/integration/test_start.py
@@ -55,6 +55,39 @@ def test_start_env_procfile(testenv):
 
 @pytest.mark.parametrize('testenv', [{
     'Procfile': 'foo: {0} test.py'.format(python_bin),
+    'Procfile.dev': 'bar: {0} test_dev.py'.format(python_bin),
+    'test.py': script,
+    'test_dev.py': textwrap.dedent("""
+        from __future__ import print_function
+        print("mongoose")
+        """)
+}], indirect=True)
+def test_start_procfile_after_command(testenv):
+    # Regression test for #173: Ensure that -f argument can be provided after
+    # command
+    ret, out, err = testenv.run_honcho(['start', '-f', 'Procfile.dev'])
+
+    assert 'mongoose' in out
+
+
+@pytest.mark.parametrize('testenv', [{
+    'Procfile': 'foo: {0} test.py'.format(python_bin),
+    'Procfile.dev': 'bar: {0} test_dev.py'.format(python_bin),
+    'test.py': script,
+    'test_dev.py': textwrap.dedent("""
+        from __future__ import print_function
+        print("mongoose")
+        """)
+}], indirect=True)
+def test_start_procfile_before_command(testenv):
+    # Test case for #173: Ensure that -f argument can be provided before command
+    ret, out, err = testenv.run_honcho(['-f', 'Procfile.dev', 'start'])
+
+    assert 'mongoose' in out
+
+
+@pytest.mark.parametrize('testenv', [{
+    'Procfile': 'foo: {0} test.py'.format(python_bin),
     'test.py': 'import sys; sys.exit(42)',
 }], indirect=True)
 def test_start_returncode(testenv):
