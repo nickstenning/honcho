3638dac092cf40ebc32819bd67aefc92f5fdc40d
Jason R. Coombs <jaraco@jaraco.com>
2018-10-24 15:24:28 -0400

    Disable test on Python 2.6.

--- tests/test_invocation.py
+++ tests/test_invocation.py
@@ -2,7 +2,10 @@ import sys
 import subprocess
 import re
 
+import pytest
 
+
+@pytest.mark.skipif(sys.version_info < (2, 7), reason="check_output not available")
 def test_runpy_invoke():
     """
     Ensure honcho can also be invoked using runpy (python -m)
