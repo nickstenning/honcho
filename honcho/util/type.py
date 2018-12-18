3b8f5718fa6ba38dddc90a6983e72f3eee8deab4
Achilles Rasquinha <achillesrasquinha@gmail.com>
2018-12-18 15:55:11 -0600

    test(component): Fix Linting Erros

--- honcho/util/type.py
+++ honcho/util/type.py
@@ -1,7 +1,7 @@
-def sequencify(value, type_ = list):
+def sequencify(value, type_=list):
     if not isinstance(value, (list, tuple)):
         value = [value]
-    
+
     value = type_(value)
 
     return value
